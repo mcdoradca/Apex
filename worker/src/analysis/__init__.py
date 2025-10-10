26504136d2e317fde8c06fcff404a82271ee3989
mcdoradca <mc.doradca@gmail.com>
2025-10-10 05:53:33 +0200

    Update __init__.py

--- worker/src/analysis/__init__.py
+++ worker/src/analysis/__init__.py
@@ -1,2 +1,12 @@
-# Ten plik celowo pozostaje pusty, aby oznaczyć ten katalog jako pakiet Pythona.
-# W przyszłości można tu umieścić importy dla całego pakietu.
+"""
+Plik inicjujący pakiet 'analysis'.
+
+Definiuje publiczne API pakietu, udostępniając kluczowe moduły,
+aby można było je łatwo importować z innych części aplikacji.
+"""
+
+from . import phase1_scanner
+from . import phase2_engine
+from . import phase3_sniper
+from . import on_demand_analyzer
+from . import utils
